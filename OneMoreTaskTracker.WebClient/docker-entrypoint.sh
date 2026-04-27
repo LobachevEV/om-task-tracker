72a9084094a74f49801e180cd4dc279eaf63e645
e.lobachev <e.lobachev@sdgroup.ai>
2026-04-27 17:27:01 +0200

    fix(compose): set empty `API_BASE_URL` to prevent double prefixing in web client

--- OneMoreTaskTracker.WebClient/docker-entrypoint.sh
+++ OneMoreTaskTracker.WebClient/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 cat > /usr/share/nginx/html/config.js <<EOF
-window.__APP_CONFIG__ = { apiBaseUrl: "${API_BASE_URL:-/api}" };
+window.__APP_CONFIG__ = { apiBaseUrl: "${API_BASE_URL:-}" };
 EOF
 
 exec nginx -g 'daemon off;'
